c234b8dbba7ece5614e4da869faec68b8c71e166
ᴇʀᴇɴxsxʏ <diegolesimarcelino@gmail.com>
2025-07-16 13:13:14 -0400

    Actualizar main-menu.js

--- plugins/main-menu.js
+++ plugins/main-menu.js
@@ -10,7 +10,7 @@ const handler = async (m, { conn }) => {
     const isPrincipal = botPrincipalJIDs.includes(conn.user?.jid)
 
     //URLs de banners hecho por Elder 
-    const bannerPrincipalURL = 'https://files.catbox.moe/5dkt6a.jpg'
+    const bannerPrincipalURL = 'https://cdn.russellxz.click/201e546d.jpeg'
     const bannerSubbotURL = 'https://cdn.russellxz.click/4badd762.jpeg' // Cambia esta URL por tu banner para subbots
 
     let text, bannerURL
